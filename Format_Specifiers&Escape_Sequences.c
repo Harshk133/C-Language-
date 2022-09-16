2b491938cc8621cce34eeefc1f4f835235e8e904
Harsh <harshmkale.2004@gmail.com>
2022-09-16 08:20:21 +0530

    Added more information.

--- Format_Specifiers&Escape_Sequences.c
+++ Format_Specifiers&Escape_Sequences.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#define PI 3.14
 
 int main(){
 printf("Hello world!");
 int a = 8;
 float b = 7.234;
+const float c = 0.123;
 printf("The Value of a is %d and b is %10.4f\n", a, b);
 printf("The Value of a is %d and b is %d\n", a, b);
 //%c - character
@@ -12,5 +14,7 @@ printf("The Value of a is %d and b is %d\n", a, b);
 //%e - long
 //%lf - double
 //%LF - long double
+
+printf("Here is the define value of PI is %d", PI);
 return 0;
 }
