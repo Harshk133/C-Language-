8834b4a66faa55c2f0d4cdfa60428fe33e7f9792
Harsh <harshmkale.2004@gmail.com>
2022-09-16 20:10:04 +0530

    added more info in repository!

---
+++ Switch_case.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+int age;
+
+printf("Enter YOur Age:\n");
+scanf("%d", &age);
+
+switch(age){
+case 3:
+printf("The Age is 3!\n");
+break;
+
+case 13:
+printf("The Age is 3!\n");
+break;
+
+case 33:
+printf("The Age is 3!\n");
+break;
+
+default:
+printf("What is your age I don't know!!");
+break;
+}
+
+return 0;
+}
