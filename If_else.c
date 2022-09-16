7a28a1cf1e657c95dc0f0eacddd97276b61e1d05
Harsh <harshmkale.2004@gmail.com>
2022-09-16 08:48:34 +0530

    Added info

--- If_else.c
+++ If_else.c
@@ -8,8 +8,11 @@ printf("Enter Your Age:\n");
 scanf("%d", &age);
 printf("You have entered %d as your age.\n", age);
 
-if(age > 18){
+if(age >= 18){
 printf("You can Vote\n");
+}
+else if(age >= 10){
+printf("Your age is between between 10 and 18!");
 }
 else{
 printf("You cannot Vote!\n");
